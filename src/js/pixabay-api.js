adeda7fd07ae833f8dd2a9a42a20f02ab08533bb
ruslana-semkiv18 <semkivruslana18@gmail.com>
2024-07-20 17:05:30 +0300

    update js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,12 +1,12 @@
 const key = "44916232-ab67e58f2cd7c4a8ed03790be";
-const url = "https://pixabay.com";
+const url = "https://pixabay.com/";
 
 axios.defaults.baseURL = url;
 
 export function getGallery({q="", page = 1, per_page = 15}={}, loader) {
   loader.style.display = "block"; 
     return axios
-    .get("api", {
+    .get("api/", {
       params: {
         key,
         q,
